66d4f44c963388f8ec5ba1319c54497d6bf59f4f
dinildamsith <dinildamsith52@gmail.com>
2024-02-05 20:49:46 +0530

    add travlign page laces and edited some files

--- src/components/Travelling/Travellign.jsx
+++ src/components/Travelling/Travellign.jsx
@@ -12,13 +12,19 @@ function Travelling(){
             <img src= {image.travel} id="travelImage" alt="travlImage"/>
             </div>
  
+            <p id="travlignAbout">
+            During my travels, I will share information about the tourist 
+            spots I visited and the areas where they are located. I have 
+            organized this section to give a clear overview. This section 
+            is dedicated to travel enthusiasts.
+            </p>
 
             <div>
                
-               <h2 id="kumariellaName">Kumari Ella</h2>
+               <h2 id="kumariellaName">&#127754;Kumari Ella</h2>
       
                   
-                  <ul class="slideshow">
+                  <ul class="kumariElla">
 
                       <li><span className="slide" style={{ backgroundImage: 'url("https://www.360view.lk/wp-content/uploads/2020/04/Kumari_Ella_Photos_By_360viewlk-1-of-5-1-scaled.jpg")' }}></span></li>
                      <li><span className="slide" style={{ backgroundImage: 'url("https://raw.githubusercontent.com/dinil2005/img/main/WhatsApp%20Image%202023-05-30%20at%205.20.08%20PM.jpeg")' }}></span></li>
@@ -45,9 +51,53 @@ function Travelling(){
                      Take precautions because the area contains several small but highly poisonous snakes, and avoid walking 
                      around barefoot. Also, please do not litter and help to keep our environment clean and healthy for future generations.
                 </p>
-               <div>
-                <Map/>
+               <div id="kuamriAllMap">
+                <Map url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.7930080563057!2d80.1045661!3d6.9116981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3abfdf24bb4f1%3A0xd244814799827974!2sW56P%2BMQP%2C%20Digana!5e0!3m2!1sen!2slk!4v1624327450682!5m2!1sen!2slk"/>
                </div>
+
+               <h2 id="barandiKovilaName">&#128509;Barandi Kovila of Seethawaka Kingdom – බැරැන්ඩි කෝවිල</h2>
+
+                <ul class="barandKovila">
+
+                      <li><span className="slide" style={{ backgroundImage: 'url("https://www.lanka-excursions-holidays.com/uploads/4/0/2/1/40216937/barandi-kovil-00-900_orig.jpg")' }}></span></li>
+                     <li><span className="slide" style={{ backgroundImage: 'url("https://raw.githubusercontent.com/dinil2005/img/main/WhatsApp%20Image%202023-04-02%20at%203.33.55%20PM.jpeg")' }}></span></li>
+                     <li><span className="slide" style={{ backgroundImage: 'url("https://i0.wp.com/amazinglanka.com/wp/wp-content/uploads/2013/10/image0436.jpg")' }}></span></li>
+                     <li><span className="slide" style={{ backgroundImage: 'url("https://i0.wp.com/amazinglanka.com/wp/wp-content/uploads/2013/10/image0440.jpg")' }}></span></li>
+                     <li><span className="slide" style={{ backgroundImage: 'url("https://i0.wp.com/amazinglanka.com/wp/wp-content/uploads/2013/10/image0423.jpg")' }}></span></li>
+
+                </ul>
+
+                <p id="barndiKoviladESC">
+                The ruins of the Barandi Kovil in a suburb of Seethawaka (formerly known as Avissawella) are 
+                rarely visited by foreigners, though many travel along the close-by main road from Kitulgala 
+                to the coast. To be honest, what can be seen of the former state temple of the 16th century is 
+                not quite imposing. However, the Barandi Kovil archaeological site is worth a short break, as the
+                 location is quite charming and the temple is of some historical significance, as it was the main 
+                 building of the former Seethawaka kingdom, which was the most important principality of the island
+                  in the second half of the 16th century. Furthermore, this is the only major state temple of a Sri
+                   Lankan monarchy that was dedicated to Shiva and thereby Hindu instead of Buddhist. Actually,
+                    Hinduism seems to have replaced Buddhism as the Sinhalese state religion in the Seethwaka period.
+                     This is the reason why the Seethawaka kingdom, though the major proponent of Sinhalese independence 
+                     against Portuguese overlordship, has not a good reputation in Buddhist historiography.
+                </p>
+
+                <div id="barandiKovilaMapp">
+                <Map url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.457696448114!2d80.2242242!3d6.9552124!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3a9aee7138b27%3A0x4f3dc7ebe60df528!2sBarandi%20Kovila!5e0!3m2!1sen!2slk!4v1707141865088!5m2!1sen!2slk"/>
+                </div>
+
+
+
+                <h2 id="kahavitaName">&#127754;Kahanawita Falls - කහනවිට ඇල්ල​</h2>
+
+                <ul class="kahnavita">
+
+                      <li><span className="slide" style={{ backgroundImage: 'url("https://i0.wp.com/amazinglanka.com/wp/wp-content/uploads/2014/05/74481063.jpg?ssl=1")' }}></span></li>
+                     <li><span className="slide" style={{ backgroundImage: 'url("https://raw.githubusercontent.com/dinil2005/img/main/photo_6168116149011723125_y.jpg")' }}></span></li>
+                     <li><span className="slide" style={{ backgroundImage: 'url("https://i0.wp.com/amazinglanka.com/wp/wp-content/uploads/2013/10/image0436.jpg")' }}></span></li>
+                     <li><span className="slide" style={{ backgroundImage: 'url("https://i0.wp.com/amazinglanka.com/wp/wp-content/uploads/2013/10/image0440.jpg")' }}></span></li>
+                     <li><span className="slide" style={{ backgroundImage: 'url("https://i0.wp.com/amazinglanka.com/wp/wp-content/uploads/2013/10/image0423.jpg")' }}></span></li>
+
+                </ul>
             </div>
         </div>
     );
--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import './map.css'
 
-const Map = () => {
+const Map = ({url}) => {
   return (
     <div>
-      <h1>Your React App</h1>
+     
       <iframe
         title="Google Maps"
         width="600"
+        
         height="450"
         loading="lazy"
         allowFullScreen
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.7930080563057!2d80.1045661!3d6.9116981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3abfdf24bb4f1%3A0xd244814799827974!2sW56P%2BMQP%2C%20Digana!5e0!3m2!1sen!2slk!4v1624327450682!5m2!1sen!2slk"
+        src={url}
       ></iframe>
     </div>
   );
