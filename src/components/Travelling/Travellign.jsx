28816c78dff3c435a4a2123ab3444b108863c42a
dinildamsith <dinildamsith52@gmail.com>
2024-02-07 02:26:28 +0530

    add mediq quary

--- src/components/Travelling/Travellign.jsx
+++ src/components/Travelling/Travellign.jsx
@@ -55,7 +55,7 @@ function Travelling(){
                 <Map url="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.7930080563057!2d80.1045661!3d6.9116981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3abfdf24bb4f1%3A0xd244814799827974!2sW56P%2BMQP%2C%20Digana!5e0!3m2!1sen!2slk!4v1624327450682!5m2!1sen!2slk"/>
                </div>
 
-               <h2 id="barandiKovilaName">&#128509;Barandi Kovila of Seethawaka Kingdom – බැරැන්ඩි කෝවිල</h2>
+               <h2 id="barandiKovilaName">&#128509;Barandi Kovila of Seethawaka Kingdom </h2>
 
                 <ul class="barandKovila">
 
@@ -87,7 +87,7 @@ function Travelling(){
 
 
 
-                <h2 id="kahavitaName">&#127754;Kahanawita Falls - කහනවිට ඇල්ල​</h2>
+                <h2 id="kahavitaName">&#127754;Kahanawita Falls​</h2>
 
                 <ul class="kahnavita">
 
